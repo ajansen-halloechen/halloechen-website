9d5472d1149ce766dea4c22f12742a6ff9d7028f
Andreas Jansen <andreas.jansen@marxkrontal.com>
2026-04-12 00:46:45 +0200

    Refactor code structure for improved readability and maintainability

--- src/calendar.ts
+++ src/calendar.ts
@@ -5,47 +5,13 @@ export type CalendarEvent = {
   description?: string;
 };
 
-const descriptionRenovation =
-  'Wir bauen um. Falls ihr Lust habt uns zu tatkräftig zu unterstützen, meldet euch gerne bei info@halloechen.de!';
-
-const descriptionInfoEvening =
-  'An diesem Abend informieren wir euch über den Stand des Genoßenschaftsprojekts Hallöchen. Kommt vorbei! Es wird auch Getränke geben.';
 const calendarEvents: CalendarEvent[] = [
   {
     id: '1',
-    timestamp: '2026-01-17T10:00:00Z',
-    title: 'Umbau',
-    description: descriptionRenovation,
-  },
-  {
-    id: '2',
-    timestamp: '2026-01-17T18:00:00Z',
-    title: 'Infoabend',
-    description: descriptionInfoEvening,
-  },
-  {
-    id: '3',
-    timestamp: '2026-01-18T10:00:00Z',
-    title: 'Umbau',
-    description: descriptionRenovation,
-  },
-  {
-    id: '4',
-    timestamp: '2026-01-24T10:00:00Z',
-    title: 'Umbau',
-    description: descriptionRenovation,
-  },
-  {
-    id: '5',
-    timestamp: '2026-01-24T18:00:00Z',
-    title: 'Infoabend',
-    description: descriptionInfoEvening,
-  },
-  {
-    id: '6',
-    timestamp: '2026-01-25T10:00:00Z',
-    title: 'Umbau',
-    description: descriptionRenovation,
+    timestamp: '2026-05-01T14:00:00Z',
+    title: 'Offene Baustelle',
+    description:
+      'Wir sind noch nicht ganz fertig, aber am 1. Mai öffnen wir schon mal die Türen für Euch! Es wird Getränke und Musik geben. Kommt rum!!!',
   },
 ];
 
