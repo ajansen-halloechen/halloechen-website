d0f4553b2af7b5e4df89098a901f3d7ec34dad66
ajansen-halloechen <info@halloechen.org>
2026-04-12 00:16:37 +0200

    colors

--- src/components/LocationAndBusinessHoursPanel.vue
+++ src/components/LocationAndBusinessHoursPanel.vue
@@ -17,29 +17,18 @@ const onMapLoad = () => {
       <div class="w-full max-w-xl aspect-[4/3] relative">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.183252228707!2d13.328274877647784!3d52.53011853547905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a851af3f3a5f61%3A0x9703c9045c49ca3a!2sHall%C3%B6chen!5e0!3m2!1sde!2sde!4v1768928723404!5m2!1sde!2sde"
-          class="w-full h-full"
-          style="border: 0"
-          allowfullscreen
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-          @load="onMapLoad"
-        />
-        <div
-          v-if="!mapLoaded"
-          class="absolute inset-0 flex items-center justify-center bg-white/70 dark:bg-slate-900/60"
-        >
-          <div
-            class="h-10 w-10 border-4 border-slate-300 border-t-slate-700 rounded-full animate-spin"
-          ></div>
+          class="w-full h-full" style="border: 0" allowfullscreen loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade" @load="onMapLoad" />
+        <div v-if="!mapLoaded"
+          class="absolute inset-0 flex items-center justify-center bg-white/70 dark:bg-slate-900/60">
+          <div class="h-10 w-10 border-4 border-slate-300 border-t-slate-700 rounded-full animate-spin"></div>
         </div>
       </div>
     </div>
     <div class="space-y-6">
       <h3 class="text-xl font-semibold">Zeit:</h3>
       <!-- Banner: Zurzeit wegen Umbau nur zu Veranstaltungen geöffnet -->
-      <div
-        class="p-2 bg-red-200 text-red-900! font-semibold rounded-md flex justify-center items-center"
-      >
+      <div class="px-4 py-2 bg-primary text-on-primary font-semibold rounded-md flex justify-center items-center">
         Momentan wegen Umbau nur zu Veranstaltungen geöffnet!
       </div>
       <table class="border-collapse max-w-xs gap-1">
