08ff5c84084d289a4af96393614994341de250b2
Andreas Jansen <andreas.jansen@marxkrontal.com>
2026-04-19 18:20:48 +0200

    refactor: align item positioning in CalendarItem and CalendarItemMd components

--- src/components/CalendarItem.vue
+++ src/components/CalendarItem.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 
 <template>
   <div class="flex items-center justify-center gap-6 mx-4">
-    <div class="flex flex-col items-end justify-center p-2">
+    <div class="flex flex-col items-center justify-center p-2">
       <div class="text-xl font-semibold leading-none">
         {{ String(new Date(props.event.timestamp).getDate()).padStart(2, '0') }}
       </div>
--- src/components/CalendarItemMd.vue
+++ src/components/CalendarItemMd.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 </script>
 <template>
   <div class="flex items-center justify-center gap-4 mx-8">
-    <div class="flex flex-col items-end justify-center p-2">
+    <div class="flex flex-col items-center justify-center p-2">
       <div class="text-xl font-semibold leading-none">
         {{ String(new Date(props.event.timestamp).getDate()).padStart(2, '0') }}
       </div>
