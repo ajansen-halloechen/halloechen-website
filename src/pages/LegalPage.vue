817f0aaf9c1c9e8ef6e17d10bdaa8db612edb4db
ajansen-halloechen <info@halloechen.org>
2026-05-03 11:07:29 +0200

    refactor: add padding to layout in LegalPage and PrivacyPage components

--- src/pages/LegalPage.vue
+++ src/pages/LegalPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-5xl mx-auto">
+  <div class="max-w-5xl mx-auto px-8">
     <h1 class="text-center text-2xl md:text-3xl font-bold py-8">Impressum</h1>
     <section class="flex flex-col gap-4 md:gap-6 text-md md:text-lg pb-10 md:pb-16">
       <p>
