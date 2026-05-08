c01faa2596a56a8e40cc2497a0b51e988aef2c4e
ajansen-halloechen <info@halloechen.org>
2026-05-08 16:54:21 +0200

    refactor: update internal navigation by adding users page and modifying events page title

--- app/layouts/internal.vue
+++ app/layouts/internal.vue
@@ -15,8 +15,7 @@ const activeItem = computed(() => {
     <div
         class="min-h-dvh flex flex-col bg-gradient-to-r from-primary/10 md:from-primary/20 via-background/95 to-primary/10 md:to-primary/20">
         <header class="sticky top-0 z-50 backdrop-blur-3xl">
-            <TopBar :items="internalNavItems" :active-item="activeItem" :show-desktop-menu="false"
-                :show-marquee="false" />
+            <TopBar :items="internalNavItems" :active-item="activeItem" :show-marquee="false" />
         </header>
 
         <main class="flex-1 w-full">
