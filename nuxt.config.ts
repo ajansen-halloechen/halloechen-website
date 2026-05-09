c3be7d4a11935488a6333a4f08b166a703142256
ajansen-halloechen <info@halloechen.org>
2026-05-09 10:18:02 +0200

    added typescript support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/main.css"],
-
+  typescript: {
+    typeCheck: true,
+  },
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
