822cbaae74cf07c7bad767186759afd51450bc94
ajansen-halloechen <info@halloechen.org>
2026-05-15 11:04:42 +0200

    feat: update color variables and improve layout styles in internal components

--- app/components/ui/AuthPanel.vue
+++ app/components/ui/AuthPanel.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="w-full grow flex items-center justify-center">
-        <div
-            class="w-full max-w-sm flex flex-col gap-4 bg-neutral-50 p-8 mx-2 border border-accent border-3 rounded-md">
+        <div class="w-full max-w-sm flex flex-col gap-4 bg-surface p-8 mx-2 border border-accent border-3 rounded-md">
             <h1 class="text-2xl font-bold text-center">
                 <slot name="heading" />
             </h1>
