522a0447108f1d34bdbee70f096fd8dd5b042e8d
Andreas Jansen <andreas.jansen@marxkrontal.com>
2026-05-15 11:04:42 +0200

    feat: update color variables and improve layout styles in internal components

--- app/layouts/internal.vue
+++ app/layouts/internal.vue
@@ -13,18 +13,12 @@ const activeItem = computed(() => {
 
 <template>
   <div
-    class="min-h-dvh flex flex-col bg-gradient-to-r from-primary/10 md:from-primary/20 via-background/95 to-primary/10 md:to-primary/20"
-  >
+    class="min-h-dvh flex flex-col bg-gradient-to-r from-primary/10 md:from-primary/20 via-background/95 to-primary/10 md:to-primary/20">
     <header class="sticky top-0 z-50 backdrop-blur-3xl">
-      <TopBar
-        :items="internalNavItems"
-        :active-item="activeItem"
-        breakpoint="xl"
-        :show-marquee="false"
-      />
+      <TopBar :items="internalNavItems" :active-item="activeItem" breakpoint="xl" :show-marquee="false" />
     </header>
 
-    <main class="flex-1 w-full">
+    <main class="flex-1 flex flex-col w-full">
       <slot />
     </main>
 
