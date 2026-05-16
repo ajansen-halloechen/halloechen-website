393d860f467ae2a6ee34a2c8cdd35297466a3597
ajansen-halloechen <info@halloechen.org>
2026-05-16 10:42:27 +0200

    using ui slots

--- app/components/ui/Page.vue
+++ app/components/ui/Page.vue
@@ -1,33 +1,46 @@
 <script setup lang="ts">
 import { tv, type VariantProps } from 'tailwind-variants';
+import { twMerge } from 'tailwind-merge';
 
 const page = tv({
-    base: 'flex flex-col w-full mx-auto px-4 md:px-8',
+    slots: {
+        root: 'flex flex-col w-full mx-auto px-4 md:px-8',
+        heading: 'text-center text-2xl md:text-3xl font-bold py-8',
+    },
     variants: {
         size: {
-            lg: 'max-w-5xl',
-            xl: 'max-w-7xl',
+            lg: { root: 'max-w-5xl' },
+            xl: { root: 'max-w-7xl' },
         }
     }
 });
 
 type PageVariants = VariantProps<typeof page>;
 
-withDefaults(
+const props = withDefaults(
     defineProps<{
         heading?: string;
         size?: PageVariants['size'];
+        ui?: Partial<Record<keyof typeof page.slots, string>>;
     }>(),
     {
         size: 'lg',
     },
 );
+
+const slots = computed(() => {
+    const { root, heading } = page({ size: props.size });
+    return {
+        root: twMerge(root(), props.ui?.root),
+        heading: twMerge(heading(), props.ui?.heading),
+    };
+});
 </script>
 
 <template>
-    <div :class="page({ size })">
+    <div :class="slots.root">
         <slot name="heading">
-            <h1 v-if="heading" class="text-center text-2xl md:text-3xl font-bold py-8">
+            <h1 v-if="heading" :class="slots.heading">
                 {{ heading }}
             </h1>
         </slot>
