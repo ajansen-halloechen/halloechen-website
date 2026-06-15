eb32b1c6c5bfecdf815f7bcae900aebd66cef801
ajansen-halloechen <info@halloechen.org>
2026-06-15 16:48:12 +0200

    styling of auth panel

--- app/components/ui/AuthPanel.vue
+++ app/components/ui/AuthPanel.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full grow flex items-center justify-center">
+  <div class="w-full py-4 grow flex items-center justify-center">
     <div
-      class="w-full max-w-sm flex flex-col gap-4 bg-surface p-8 mx-2 border border-accent border-3 rounded-md"
+      class="w-full max-w-md flex flex-col gap-4 bg-surface p-4 mx-2 border border-accent border-3 rounded-md"
     >
       <h1 class="text-2xl font-bold text-center">
         <slot name="heading" />
