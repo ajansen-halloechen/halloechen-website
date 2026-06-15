63c15a923f57396d1d2f3659626a4fb25a377d01
ajansen-halloechen <info@halloechen.org>
2026-06-15 10:50:18 +0200

    small change to Icon Button

--- app/components/ui/IconButton.vue
+++ app/components/ui/IconButton.vue
@@ -8,7 +8,7 @@ const iconButton = tv({
   variants: {
     variant: {
       plain: 'text-primary hover:bg-primary/70 hover:text-accent',
-      nohover: 'text-primary',
+      nohover: 'text-current',
       outlined:
         'bg-transparent border border-primary text-primary hover:bg-primary/10',
       solid: 'bg-primary text-accent hover:bg-primary-900',
