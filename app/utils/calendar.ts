d1729e500d47e890127873a289959b9f9885e345
ajansen-halloechen <info@halloechen.org>
2026-06-29 19:58:54 +0200

    updated calender

--- app/utils/calendar.ts
+++ app/utils/calendar.ts
@@ -20,6 +20,13 @@ const calendarEvents: CalendarEvent[] = [
     description:
       'Es ist endlich soweit! Ab 30. Mai haben wir regulär geöffnet. Kommt vorbei! Wir freuen uns RIESIG.',
   },
+  {
+    id: '3',
+    timestamp: '2026-06-21T13:00:00Z',
+    title: 'Fête de la Musique',
+    description:
+      'Wir haben drei Acts für Euch: TRAD BURMAWI - Singer/Songwriter aus der Nachbarschaft, jordanischer Folk; BREAKABLES - Syntheziser Duo aus Helsinki, Richtung Elektro; ByDS - Syntheziser Duo aus Mexico City, Fokus Cumbia. Weiterhin wird es wahrscheinlich auch etwas aufgelegte Musik geben!',
+  },
 ];
 
 export function getCalendarEvents(start: Date, end: Date): CalendarEvent[] {
